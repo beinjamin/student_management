8ce1a1c3503f912207520852e8f2c381bd35d9b9
beinjamin <jauresbeinjamin@gmail.com>
2021-10-31 01:39:16 +0100

    Creation views  etudiants

---
+++ resources/views/etudiant.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="x-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+   <h1>la Liste des etudiant</h1>
+</html>
