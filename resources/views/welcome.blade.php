0f34fb239b35e31cebd7d9fbb11a4589cddba3c4
beinjamin <jauresbeinjamin@gmail.com>
2021-10-31 02:12:45 +0100

    Insertion du js sur la views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -184,7 +184,7 @@
 </main>
 
 
-    <script src="/docs/5.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
       <script src="offcanvas.js"></script>
   </body>
