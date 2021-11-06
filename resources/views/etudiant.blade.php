225ccbce93e938873fce2407f7eceb020157d762
beinjamin <jauresbeinjamin@gmail.com>
2021-11-06 22:44:25 +0100

    Mise en place des etudiants dans la views etudiant depuis la b.d

--- resources/views/etudiant.blade.php
+++ resources/views/etudiant.blade.php
@@ -22,16 +22,20 @@
   </thead>
   <tbody>
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+    @foreach ($etudiants as $etudiant )
+
+    @endforeach
+      <th scope="row">{{ $etudiant->id }}</th>
+      <td>{{ $etudiant->nom }}</td>
+      <td>{{ $etudiant->prenom }}</td>
+      <td>{{ $etudiant->classe }}</td>
 
     <td>
        <a hef="#" class="btn btn-info">Modifiers</a>
        <a hef="#" class="btn btn-danger">Supprimer</a>
     </td>
      </tr>
+  @endforeach
   </tbody>
 </table>
 
