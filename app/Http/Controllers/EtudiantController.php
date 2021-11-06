d13becb253f2ca18f0cb3e0e2ed2fd8d4febc92e
beinjamin <jauresbeinjamin@gmail.com>
2021-11-06 22:35:06 +0100

    Mise en place d'une methode dans le controlleur pour afficher la liste des etudiant de maniere alphabetique

--- app/Http/Controllers/EtudiantController.php
+++ app/Http/Controllers/EtudiantController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Etudiant;
 use Illuminate\Http\Request;
 
 class EtudiantController extends Controller
 {
-    public function index(){
-    return view ("etudiant");
+    public function index()
+    {
+        $liste_etudiants = Etudiant::orderBy("nom", "asc")->get();
+        return view("etudiant");
     }
 }
