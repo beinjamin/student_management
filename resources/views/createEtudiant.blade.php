31310bbb2ce82918a54e92bc12337ae29a15f37a
beinjamin <jauresbeinjamin@gmail.com>
2021-11-08 17:56:31 +0100

    Gestion de la view createEtudiant et mise en place du formulaire

--- resources/views/createEtudiant.blade.php
+++ resources/views/createEtudiant.blade.php
@@ -9,18 +9,28 @@
     <div class="mt-4">
 
 <form>
+
+@csrf
   <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    <label for="exampleInputEmail1" class="form-label">Nom de l'etudiant</label>
+    <input type="text" class="form-control" >
   </div>
+
+
   <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <label for="exampleInputPassword1" class="form-label">Prénom</label>
+    <input type="password" class="form-control">
   </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+
+
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Classe</label>
+    <select class="form-control">
+    <option value=""></option>
+    @foreach ($classes as $classe )
+    <option value="{{$classe->id}}">{{$classe->libelle}} </option>
+    @endforeach
+    <select>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
